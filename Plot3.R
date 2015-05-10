a9f6d899b7208ca1772d5f06b21311bc00327f54
Jesper Venema <jesper.venema@gmail.com>
2015-05-10 14:29:06 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -35,5 +35,5 @@ plot(dbn$DateTime,dbn$Sub_metering_1,type="l", col="red", ylab="Energy sub meter
 lines(dbn$DateTime,dbn$Sub_metering_2,type="l", col="green")
 lines(dbn$DateTime,dbn$Sub_metering_3,type="l", col="blue")
 legend("top", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n", lty=1, col=c("red", "green", "blue"))
-dev.copy(png,'plot3.png')
+dev.copy(png,'plot3.png',width = 480, height = 480)
 dev.off()
