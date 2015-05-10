82b3d34fdfd86932c499c21bc1842773a9cf134d
Jesper Venema <jesper.venema@gmail.com>
2015-05-10 14:29:35 -0400

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -44,5 +44,5 @@ legend("top", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n", lt
 
 plot(dbn$DateTime,dbn$Global_reactive_power,type="l", ylab="Global_reactive_power", xlab="datetime")
 
-dev.copy(png,'plot4.png')
+dev.copy(png,'plot4.png',width = 480, height = 480)
 dev.off()
