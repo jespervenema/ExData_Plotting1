5c39d21754c7f02f5c6f62cc2a8477378c3d11f7
Jesper Venema <jesper.venema@gmail.com>
2015-05-10 14:18:08 -0400

    Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,39 @@
+db = read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+
+db$DateTime <- paste(db$Date,db$Time, sep = ":")
+db$DateTime = strptime(db$DateTime,format="%d/%m/%Y:%H:%M:%S")
+
+db$Date <- as.Date(db$Date, format="%d/%m/%Y")
+
+
+dbn <- subset(db, db$Date == "2007/02/01" | db$Date == "2007/02/02")
+
+dbn$Global_active_power[dbn$Global_active_power=="?"] <- NA
+dbn$Global_reactive_power[dbn$Global_reactive_power=="?"] <- NA
+dbn$Sub_metering_1[dbn$Sub_metering_1=="?"] <- NA
+dbn$Sub_metering_2[dbn$Sub_metering_2=="?"] <- NA
+dbn$Sub_metering_3[dbn$Sub_metering_3=="?"] <- NA
+dbn$Voltage[dbn$Voltage=="?"] <- NA
+dbn$Global_intensity[dbn$Global_intensity=="?"] <- NA
+dbn$Global_intensity <- gsub(",", "", dbn$Global_intensity)
+dbn$Global_active_power <- gsub(",", "", dbn$Global_active_power)
+dbn$Global_reactive_power <- gsub(",", "", dbn$Global_reactive_power)
+dbn$Voltage <- gsub(",", "", dbn$Voltage)
+dbn$Sub_metering_1 <- gsub(",", "", dbn$Sub_metering_1)
+dbn$Sub_metering_2 <- gsub(",", "", dbn$Sub_metering_2)
+dbn$Sub_metering_3 <- gsub(",", "", dbn$Sub_metering_3)
+dbn$Global_intensity <- as.numeric(dbn$Global_intensity)
+dbn$Global_active_power <- as.numeric(dbn$Global_active_power)
+dbn$Global_reactive_power <- as.numeric(dbn$Global_reactive_power)
+dbn$Voltage <- as.numeric(dbn$Voltage)
+dbn$Sub_metering_1 <- as.numeric(dbn$Sub_metering_1)
+dbn$Sub_metering_2 <- as.numeric(dbn$Sub_metering_2)
+dbn$Sub_metering_3 <- as.numeric(dbn$Sub_metering_3)
+
+
+plot(dbn$DateTime,dbn$Sub_metering_1,type="l", col="red", ylab="Energy sub metering", xlab="")
+lines(dbn$DateTime,dbn$Sub_metering_2,type="l", col="green")
+lines(dbn$DateTime,dbn$Sub_metering_3,type="l", col="blue")
+legend("top", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n", lty=1, col=c("red", "green", "blue"))
+dev.copy(png,'plot3.png')
+dev.off()
